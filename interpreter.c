e8f71cdb94d265b7db90768e030c6147dd3c6207
Danco1201 <fr4nte.del.sur@gmail.com>
2025-01-27 10:07:17 -0300

    .

--- interpreter.c
+++ interpreter.c
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 
 #define MEMORY_SIZE 30000
+
 typedef struct {
     char name[50];
     void (*func)(int*, int*);
 } function;
-// funciones en desarrollo, pero se escribiran como [x + 2 3], que seria en python: x=lambda x, y: x + y; x(2, 3)
+
 void move_left(int* pointer) {
     (*pointer)--;
 }
@@ -15,8 +16,6 @@ void move_left(int* pointer) {
 void move_right(int* pointer) {
     (*pointer)++;
 }
-
-void release_pointer() {}
 
 void increment_memory(int* memory, int pointer) {
     memory[pointer]++;
@@ -29,45 +28,89 @@ void decrement_memory(int* memory, int pointer) {
 void repl(int* memory, int pointer) {
     printf("%c", memory[pointer]);
 }
+
+void input(int* memory, int pointer) {
+    char c = getchar();
+    memory[pointer] = c;
+}
+
+void reset_memory(int* memory, int pointer) {
+    memory[pointer] = 0;
+}
+
+void jump_to_start(int* pointer) {
+    *pointer = 0;
+}
+
 int condition(int* memory, int pointer) {
     return memory[pointer] != 0; 
 }
+
 void interpret(const char* code) {
     int memory[MEMORY_SIZE] = {0};
     int pointer = 0;
 
-    char command[50];
     const char* delimiter = ",";
-
     char* code_copy = strdup(code);
     char* instruction = strtok(code_copy, delimiter);
 
     while (instruction != NULL) {
-        while (*instruction == ' ') instruction++;  
+        while (*instruction == ' ') instruction++;  // Ignorar espacios al inicio
+
         if (strcmp(instruction, "bye") == 0) {
             break;
-        } else if (strchr(instruction, '[')) {
-            // a
+        } else if (strstr(instruction, "[salve") != NULL) {
+            char loop_body[200];
+            if (sscanf(instruction, "[salve %199[^]]", loop_body) == 1) {
+                char* inner_instruction;
+                do {
+                    char* body_copy = strdup(loop_body);
+                    inner_instruction = strtok(body_copy, delimiter);
+                    while (inner_instruction != NULL) {
+                        while (*inner_instruction == ' ') inner_instruction++; // Ignorar espacios
+
+                        if (strcmp(inner_instruction, "hi") == 0) {
+                            move_left(&pointer);
+                        } else if (strcmp(inner_instruction, "hello") == 0) {
+                            move_right(&pointer);
+                        } else if (strcmp(inner_instruction, "hola") == 0) {
+                            increment_memory(memory, pointer);
+                        } else if (strcmp(inner_instruction, "hallo") == 0) {
+                            decrement_memory(memory, pointer);
+                        } else if (strcmp(inner_instruction, "bonjour") == 0) {
+                            repl(memory, pointer);
+                        } else if (strcmp(inner_instruction, "ciao") == 0) {
+                            input(memory, pointer);
+                        } else if (strcmp(inner_instruction, "privet") == 0) {
+                            reset_memory(memory, pointer);
+                        } else if (strcmp(inner_instruction, "ola") == 0) {
+                            jump_to_start(&pointer);
+                        } else {
+                            printf("unknown command in loop: %s\n", inner_instruction);
+                        }
+
+                        inner_instruction = strtok(NULL, delimiter);
+                    }
+                    free(body_copy);
+                } while (condition(memory, pointer));
+            }
         } else {
             if (strcmp(instruction, "hi") == 0) {
                 move_left(&pointer);
             } else if (strcmp(instruction, "hello") == 0) {
                 move_right(&pointer);
-            } else if (strcmp(instruction, "hey") == 0) {
-                release_pointer();
             } else if (strcmp(instruction, "hola") == 0) {
                 increment_memory(memory, pointer);
             } else if (strcmp(instruction, "hallo") == 0) {
                 decrement_memory(memory, pointer);
             } else if (strcmp(instruction, "bonjour") == 0) {
                 repl(memory, pointer);
-            } else if (strstr(instruction, "[salve") != NULL) {
-                char condition[50], body[200];
-                if (sscanf(instruction, "[salve %s]", condition) == 1) {
-                    while (condition(memory, pointer)) {
-                        move_right(&pointer);
-                    }
-                }
+            } else if (strcmp(instruction, "ciao") == 0) {
+                input(memory, pointer);
+            } else if (strcmp(instruction, "privet") == 0) {
+                reset_memory(memory, pointer);
+            } else if (strcmp(instruction, "ola") == 0) {
+                jump_to_start(&pointer);
             } else {
                 printf("unknown command: %s\n", instruction);
             }
